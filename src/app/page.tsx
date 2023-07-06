a6ea185cfeb6fc7d3a42003f476e47d27eea2ee0
Eddie SanMarco <esanmarco@linksquares.com>
2023-07-06 14:02:07 -0700

    added Todos to files for next session

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,23 @@
 import CreatePoll from "@/components/forms/createPoll";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+/**
+ * Todo:
+ * - Fetch a list of public polls stored in the database
+ * - Link the display of polls to a details page
+ * - Validate revalidating the cache is working (https://nextjs.org/docs/app/building-your-application/data-fetching/revalidating#using-on-demand-revalidation)
+ */
 
 export default function Home() {
   return (
-    <div className="p-8">
-      <Card className="w-full max-w-xl mx-auto rounded">
-        <CardHeader>
-          <CardTitle>Create New Poll</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <CreatePoll />
-        </CardContent>
-      </Card>
+    <div className="flex flex-row h-full gap-10 p-4 py-8">
+      <aside className="h-full p-6 rounded-md shadow-lg w-60">
+        <h4 className="mb-4 text-sm font-medium leading-none">
+          Create New Poll
+        </h4>
+        <CreatePoll />
+      </aside>
+
+      <div className="flex-1">Display Public Polls Here</div>
     </div>
   );
 }
--- src/components/forms/createPoll.tsx
+++ src/components/forms/createPoll.tsx
@@ -11,39 +11,46 @@ const pollSchema = z.object({
 
 export type Poll = z.infer<typeof pollSchema>;
 
+/**
+ * Todo:
+ * - convert change states to internal state instead of form post.
+ * - Add "email address" field to form
+ * - Handle adding additional options
+ * - Add labeling to form
+ * - Handle data validation & submission / rehydration
+ */
+
 export default function CreatePoll() {
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const formData = new FormData(e.target as HTMLFormElement);
+
+    const formData = new FormData(e.currentTarget);
     const data = Object.fromEntries(formData.entries());
 
-    const res = await fetch(`http//localhost:3000/api/polls`, {
-      method: "POST",
-      body: JSON.stringify(data),
-    });
+    console.log(data);
 
-    if (!res.ok) {
-      throw new Error("Something went wrong");
-    }
+    // const res = await fetch(`http//localhost:3000/api/polls`, {
+    //   method: "POST",
+    //   body: JSON.stringify(data),
+    // });
 
-    const poll = await res.json();
-    console.log(poll);
+    // if (!res.ok) {
+    //   throw new Error("Something went wrong");
+    // }
+
+    // const poll = await res.json();
+    // console.log(poll);
   };
 
   return (
     <form onSubmit={handleSubmit} className="flex flex-col gap-5">
-      <Input type="text" placeholder="Poll Title" />
+      <Input name="title" type="text" placeholder="Poll Title" />
       <hr />
 
       <Input type="text" placeholder="Option 1" />
       <Input type="text" placeholder="Option 2" />
 
-      <Button
-        variant="outline"
-        color="secondary"
-        className="w-fit"
-        disabled={!pollSchema.safeParse({ title: "", options: [] }).success}
-      >
+      <Button variant="outline" color="secondary" className="w-fit">
         Create Poll
       </Button>
     </form>
